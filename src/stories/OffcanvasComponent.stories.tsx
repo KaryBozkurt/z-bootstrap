0c71d8db968226ac3c394484ee8472d4beaa5424
elliehachemjob <elliehachemjob@gmail.com>
2021-11-03 14:14:49 +0200

    offcanvas component doc finished

--- src/stories/OffcanvasComponent.stories.tsx
+++ src/stories/OffcanvasComponent.stories.tsx
@@ -4,6 +4,40 @@ import { OffcanvasComponent } from '../components/CommonComponents';
 export default {
   title: 'Z-BootStrap/OffcanvasComponent',
   component: OffcanvasComponent,
+  argTypes: {
+    ButtonOnClickShowCanvas: {
+      description:
+        'Function to Show/UnShow the canvas when clicked  when clicked  ',
+    },
+    OffcanvasOnClickShowCanvas: {
+      description:
+        'Function to Show/UnShow the canvas when clicked  on cancel button or outside canvas   ',
+    },
+    variant: {
+      description: 'Sets the color of the button   ',
+    },
+    size: {
+      description: 'Sets the size of the button   ',
+    },
+    disabled: {
+      description: 'Checks if button is disabled or not   ',
+    },
+    active: {
+      description: 'Checks if button is active or not   ',
+    },
+    isOffCanvas: {
+      description: 'Sets if canvas is off or on   ',
+    },
+    launchOffcanvasText: {
+      description: 'Sets the text of button launched canvas   ',
+    },
+    OffcanvasTitle: {
+      description: 'Sets the title text of the canvas   ',
+    },
+    OffcanvasBodyDescription: {
+      description: 'Sets the description text of the canvas  ',
+    },
+  },
 };
 
 const OffcanvasComponentTemplate = (args: any) => (
